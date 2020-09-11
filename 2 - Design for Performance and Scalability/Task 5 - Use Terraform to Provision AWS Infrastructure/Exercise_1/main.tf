9f9c14143d8705a5b038800fd38ec8f582f54f71
sameer <sameer.barha12@gmail.com>
2020-09-11 23:17:18 +0530

    updated subnet

--- 2 - Design for Performance and Scalability/Task 5 - Use Terraform to Provision AWS Infrastructure/Exercise_1/main.tf
+++ 2 - Design for Performance and Scalability/Task 5 - Use Terraform to Provision AWS Infrastructure/Exercise_1/main.tf
@@ -1,7 +1,7 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
     access_key = "Enter Access Key"
-    secret_key = "Enter Secret key"
+    secret_key = "Enter Secret Key"
     region     = "ap-south-1"
 }
 
@@ -10,8 +10,8 @@ resource "aws_instance" "Udacity_T2" {
     ami = "ami-09052aa9bc337c78d"
     instance_type = "t2.micro"
     count = "4"
-    vpc_security_group_ids=["sg-06ad200d92b646b51"]
-    subnet_id="subnet-019d37aa4d1abd20a"
+    vpc_security_group_ids=["sg-f4d99696"]
+    subnet_id="subnet-8aea81c6"
     availability_zone ="ap-south-1b"
     tags = {
         Name = "Udacity T2"
@@ -24,8 +24,8 @@ resource "aws_instance" "Udacity_M4" {
     ami = "ami-09052aa9bc337c78d"
     count = "2"
     instance_type = "m4.large"
-    vpc_security_group_ids=["sg-06ad200d92b646b51"]
-    subnet_id="subnet-019d37aa4d1abd20a"
+    vpc_security_group_ids=["sg-f4d99696"]
+    subnet_id="subnet-8aea81c6"
     availability_zone ="ap-south-1b"
     tags = {
       Name = "Udacity M4"
